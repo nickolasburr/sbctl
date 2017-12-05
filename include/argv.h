08f5178f3e48f516b6493204da378a3531921e95
Nickolas Burr <nickolasburr@gmail.com>
2017-12-04 21:59:13 -0500

    Add command_t, bitmask macros, update usage

--- include/argv.h
+++ include/argv.h
@@ -11,18 +11,36 @@
 #include "error.h"
 #include "utils.h"
 
-#define NUM_OPTS 2
+#define NUM_CMDS 2
+
+/**
+ * Hex masks for commands, options.
+ */
+#define MASK_CMD_HELP 0x1
+#define MASK_CMD_VERS 0x2
+#define MASK_CMD_LIST 0x4
+#define MASK_CMD_GET  0x8
+#define MASK_CMD_SET  0x10
 
 typedef struct {
 	char *value;
 	char *alias;
 	char *desc;
-	int mask;
+	int bitmask;
 } option_t;
 
-static option_t options[NUM_OPTS];
+typedef struct {
+	char *value;
+	char *alias;
+	char *desc;
+	int bitmask;
+	option_t *options;
+} command_t;
 
-int get_mask_from_value(char *);
+static command_t commands[NUM_CMDS];
+
+int get_command_bitmask(char *);
+int get_option_bitmask(char *);
 void usage(void);
 
 #endif /* SBCTL_ARGV_H */
