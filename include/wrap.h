3b1a41237a9bc57c9ca7f7119920fae697d53272
Nickolas Burr <nickolasburr@gmail.com>
2017-12-08 17:16:01 -0500

    Add Kernel cpp wrapper header

---
+++ include/wrap.h
@@ -0,0 +1,20 @@
+/**
+ * wrap.h
+ *
+ * Copyright (C) 2017 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#ifndef SBCTL_WRAP_H
+#define SBCTL_WRAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Kernel/IOKit/pci/IOPCIDevice.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SBCTL_WRAP_H */
