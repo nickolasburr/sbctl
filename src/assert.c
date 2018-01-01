ffdb929cbc6c49cd02909ac6b0d2d2d8b2b5dc2c
Nickolas Burr <nickolasburr@gmail.com>
2018-01-01 15:52:56 -0500

    Add assert interface

---
+++ src/assert.c
@@ -0,0 +1,15 @@
+/**
+ * assert.c
+ *
+ * Copyright (C) 2017 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#include "assert.h"
+
+const Except_T Assert_Failed = {
+	"Assertion failed"
+};
+
+void (assert)(int e) {
+	assert(e);
+}
